484f0999b328cd420e93468e7387674306decd0d
Benjamin Worthington <benworthin@icloud.com>
2021-01-29 06:47:04 -0600

    Started UI in main

--- main.cpp
+++ main.cpp
@@ -5,10 +5,37 @@
 #include <iostream>
 using namespace std;
 
+void printCommandMenu();
+void newReviewMenu();
+
 int main() {
 
-    cout << "Hello, World!" << endl;
+    bool stop = false;
+
+    cout << "Ben's Foodie Reviews" << endl;
+    cout << endl;
+    printCommandMenu();
+
+    while (stop) {
+
+    }
 
     return 0;
+
+}
+
+void printCommandMenu() {
+
+    cout << "Command Menu:" << endl;
+    cout <<"\t1. Enter new review" << endl;
+    cout <<"\t2. Print specified Restaurant reviews" << endl;
+    cout << "\t3. Print specified food category reviews" << endl;
+    cout << "\t4. Print recent reviews" << endl;
+
+}
+
+void newReviewMenu() {
+
+
 
 }
