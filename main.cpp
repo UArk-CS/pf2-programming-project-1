269f74197ccd3db8718f7b4ad845cd6f9189e92e
Benjamin Worthington <benworthin@icloud.com>
2021-01-29 09:08:43 -0600

    Implemented user input for the command menu

--- main.cpp
+++ main.cpp
@@ -5,18 +5,47 @@
 #include <iostream>
 using namespace std;
 
-void printCommandMenu();
+void commandMenu(int &menuChoice);
 void newReviewMenu();
 
 int main() {
 
     bool stop = false;
+    int menuChoice = 0;
 
     cout << "Ben's Foodie Reviews" << endl;
     cout << endl;
-    printCommandMenu();
 
-    while (stop) {
+    while (!stop) {
+
+        commandMenu(menuChoice);
+
+        switch (menuChoice) {
+
+            case 1:
+                // New review
+                cout << "New review case" << endl;
+                break;
+            case 2:
+                // Print specified restaurant reviews
+                cout << "Print restaurant reviews case" << endl;
+                break;
+            case 3:
+                // Print specified food category reviews
+                cout << "Print food cateogry reviews case" << endl;
+                break;
+            case 4:
+                // Print recent reviews
+                cout << "Print recent reviews case" << endl;
+                break;
+            case 5:
+                // Quit
+                stop = true;
+                break;
+            default:
+                break;
+
+        }
 
     }
 
@@ -24,13 +53,27 @@ int main() {
 
 }
 
-void printCommandMenu() {
+void commandMenu(int &menuChoice) {
 
     cout << "Command Menu:" << endl;
     cout <<"\t1. Enter new review" << endl;
     cout <<"\t2. Print specified Restaurant reviews" << endl;
     cout << "\t3. Print specified food category reviews" << endl;
     cout << "\t4. Print recent reviews" << endl;
+    cout << "\t5. Quit" << endl;
+
+    cout << "Enter a selection (1-5): > ";
+    cin >> menuChoice;
+
+    if (menuChoice < 1 || menuChoice > 5) {
+
+        cout << "Invalid selection. Please try again." << endl;
+        cout << endl;
+        commandMenu(menuChoice);
+
+    }
+
+    cout << endl;
 
 }
 
